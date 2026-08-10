aa1e346f0eb47f75d71bdd76c7c2ed8e467f444e
NeonCranberries <benjaminalexander898@gmail.com>
2026-08-10 00:20:59 -0500

    8.0.5

--- src/main/java/com/cranberry/meed/MeedMod.java
+++ src/main/java/com/cranberry/meed/MeedMod.java
@@ -1,8 +1,6 @@
 package com.cranberry.meed;
 
 import com.cranberry.meed.condition.ModConditions;
-import com.cranberry.meed.network.ModNetwork;
-import com.cranberry.meed.server.MeedCommands;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
@@ -16,8 +14,6 @@ public class MeedMod {
 
         ModConditions.CONDITION_CODECS.register(modEventBus);
 
-        ModNetwork.register(modEventBus);
-        MeedCommands.register();
-
+        // client command registers itself via @EventBusSubscriber
     }
 }
